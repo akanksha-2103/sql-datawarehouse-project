d53bb9b628bff8d27674501b986d58de4820a85a
Gangam Akanksha <akankshareddygangam520@gmail.com>
2025-10-31 10:41:07 +0530

    SqlQueries.sql

--- scripts/SqlQueries.sql
+++ scripts/SqlQueries.sql
@@ -38,7 +38,7 @@ prd_end_dt DATETIME
 );
 
 
-LOAD DATA LOCAL INFILE '/Users/gangamakankshareddy/Downloads/datasets/source_crm/prd_info.csv'INTO TABLE bronze.crm_cust_info
+LOAD DATA LOCAL INFILE '/Users/gangamakankshareddy/Downloads/datasets/source_crm/prd_info.csv'INTO TABLE bronze.crm_prd_info
 FIELDS TERMINATED BY ','
 IGNORE 1 ROWS;
 
@@ -57,7 +57,7 @@ sls_price INT
 );
 
 
-LOAD DATA LOCAL INFILE '/Users/gangamakankshareddy/Downloads/datasets/source_crm/sales_details.csv'INTO TABLE bronze.crm_cust_info
+LOAD DATA LOCAL INFILE '/Users/gangamakankshareddy/Downloads/datasets/source_crm/sales_details.csv'INTO TABLE bronze.crm_sales_details
 FIELDS TERMINATED BY ','
 IGNORE 1 ROWS;
 
@@ -69,7 +69,7 @@ bdate DATE,
 gen varchar(50)
 );
 
-LOAD DATA LOCAL INFILE '/Users/gangamakankshareddy/Downloads/datasets/source_erp/CUST_AZ12.csv'INTO TABLE bronze.crm_cust_info
+LOAD DATA LOCAL INFILE '/Users/gangamakankshareddy/Downloads/datasets/source_erp/CUST_AZ12.csv'INTO TABLE bronze.erp_cust_az12
 FIELDS TERMINATED BY ','
 IGNORE 1 ROWS;
 
@@ -80,7 +80,7 @@ cntry varchar(50)
 );
 
 
-LOAD DATA LOCAL INFILE '/Users/gangamakankshareddy/Downloads/datasets/source_erp/LOC_A101.csv'INTO TABLE bronze.crm_cust_info
+LOAD DATA LOCAL INFILE '/Users/gangamakankshareddy/Downloads/datasets/source_erp/LOC_A101.csv'INTO TABLE bronze.erp_loc_a101
 FIELDS TERMINATED BY ','
 IGNORE 1 ROWS;
 
@@ -94,6 +94,6 @@ maintenance varchar(50)
 );
 
 
-LOAD DATA LOCAL INFILE '/Users/gangamakankshareddy/Downloads/datasets/source_erp/PX_CAT_G1V2.csv'INTO TABLE bronze.crm_cust_info
+LOAD DATA LOCAL INFILE '/Users/gangamakankshareddy/Downloads/datasets/source_erp/PX_CAT_G1V2.csv'INTO TABLE bronze.px_cat_g1v2
 FIELDS TERMINATED BY ','
 IGNORE 1 ROWS;
