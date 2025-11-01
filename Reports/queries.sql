006297ff6f95ea0155e5038d3484eb2c8a18455e
Gangam Akanksha <akankshareddygangam520@gmail.com>
2025-11-01 21:28:50 +0530

    Update queries.sql

--- Reports/queries.sql
+++ Reports/queries.sql
@@ -64,6 +64,85 @@ customer_aggregation AS (
 
 SELECT * FROM customer_aggregation;
 
-
-
 SELECT*FROM gold.report_customers;
+
+
+
+--- GOLD.REPORT_PRODUCTS ----
+
+drop  view if exists gold.report_products;
+
+create view gold.report_products;
+
+WITH base_query AS (
+    SELECT
+        p.product_key,
+        p.product_name,
+        p.category,
+        p.subcategory,
+        p.cost,
+        f.order_name,
+        f.order_date,
+        f.customer_key,
+        f.sales_amount,
+        f.quantity
+    FROM gold.fact_sales f
+    LEFT JOIN gold.dim_products p
+        ON f.product_key = p.product_key
+    WHERE order_date IS NOT NULL
+),
+product_aggregation AS (
+    SELECT
+        product_key,
+        product_name,
+        category,
+        subcategory,
+        cost,
+        TIMESTAMPDIFF(MONTH, MIN(order_date), MAX(order_date)) AS lifespan,
+        MAX(order_date) AS last_sale_date,
+        COUNT(DISTINCT order_name) AS total_orders,
+        COUNT(DISTINCT customer_key) AS total_customers,
+        SUM(sales_amount) AS total_sales,
+        SUM(quantity) AS total_quantity,
+        ROUND( AVG( CAST(sales_amount AS DECIMAL(18,4)) / NULLIF(quantity,0) ), 1) AS avg_selling_price
+    FROM base_query
+    GROUP BY
+        product_key,
+        product_name,
+        category,
+        subcategory,
+        cost
+)
+SELECT 
+        product_key,
+        product_name,
+        category,
+        subcategory,
+        cost,
+        last_sale_date,
+        timestampdiff(month,last_sale_date,now()) as recency_in_months,
+        CASE
+            when total_sales>50000 then 'high-performer'
+            when total_sales>= 10000 then 'mid- range'
+            else 'low-performer'
+		end as product_segment,
+        lifespan,
+        total_orders,
+        total_sales,
+        total_quantity,
+        total_customers,
+        avg_selling_price,
+        CASE
+            when total_orders=0 then 0
+            else total_sales/total_orders
+		end as avg_order_revenue,
+        
+        case
+           when lifespan=0 then total_sales
+           else total_sales/lifespan
+		end as avg_monthly_revenue
+        from product_aggregation;
+        
+        
+
+select * from gold.report_products;
